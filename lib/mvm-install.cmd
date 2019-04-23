185c24bcc4ac66c970f394700beb60b764d6f541
ometaxas <ometaxas@localhost>
2019-04-23 17:41:47 +0300

    External libs

---
+++ lib/mvm-install.cmd
@@ -0,0 +1,6 @@
+
+mvn install:install-file -Dfile=StringHelpers-0.0.1.jar -DgroupId=com.sree.textbytes -DartifactId=StringHelpers -Dversion=0.0.1 -Dpackaging=jar
+
+mvn install:install-file -Dfile=jtopia-0.0.3.jar -DgroupId=com.sree.textbytes -DartifactId=jtopia -Dversion=0.0.3 -Dpackaging=jar
+
+
